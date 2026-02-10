69a9b86026cde4ecb08a42dac1c908edbd977996
mel22083 <mel22083@uvg.edu.gt>
2026-02-10 16:49:28 -0600

    Create ADC.h

---
+++ ADC.h
@@ -0,0 +1,15 @@
+/*
+ * ADC.h
+ *
+ * Created: 1/27/2026 10:57:00 AM
+ *  Author: willi
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+void init_ADC(void);
+uint16_t leer_ADC(uint8_t canal);
+
+#endif /* ADC_H_ */
